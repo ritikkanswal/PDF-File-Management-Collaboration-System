c0720dbba41eb465176c6bd1bcbdd82352f7a828
Ritik Kanswal <ritik.1@spinny.com>
2023-06-05 16:24:53 +0530

    aloowed hosts added

--- pdf_management_collaboration_system/settings.py
+++ pdf_management_collaboration_system/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v)n2#6-4b6k!2uwdp=@-we%oga$0%ujn_@7)qdov!rm3a-*j2v'
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
