0f315767708568d77e116e802f2695f260877b2f
Ritik Kanswal <ritik.kanswal.5@gmail.com>
2023-06-05 18:42:50 +0000

    bugx fixed

--- pdf_management_collaboration_system/settings.py
+++ pdf_management_collaboration_system/settings.py
@@ -148,11 +148,13 @@ AUTH_USER_MODEL = "app.User"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- pdf_management_collaboration_system/urls.py
+++ pdf_management_collaboration_system/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 from django.views.generic import TemplateView
-
+from django.conf.urls.static import static  # new
+from django.conf import settings  # new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.HomePage,name='home'),
@@ -23,3 +24,8 @@ urlpatterns = [
     path('api/share/', views.share, name='share'),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
